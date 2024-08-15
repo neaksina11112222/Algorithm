2d97f0b0fcebc9faefc3909a12013f29074ad7e4
neaksina11112222 <neaksina752@gmail.com>
2024-08-15 20:55:51 +0700

    Add files via upload

---
+++ excersice.py
@@ -0,0 +1,12 @@
+score = int(input('enter your num: '))
+
+if score < 50:
+       print('bad')
+elif score > 50 and score <= 79:
+       print('Not bad, Not good')
+elif score > 80 and score <= 100:
+       print('Excelnt')
+
+ 
+else:
+       print('You are crazy')
